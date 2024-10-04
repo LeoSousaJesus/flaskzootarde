ff5529c4001d187170d1b55cb0dede0bf541c5c7
Leandro <leandrosousajesus@gmail.com>
2024-10-04 18:09:43 -0300

    Aula ZooFlask

---
+++ zooflasktarde.sql
@@ -0,0 +1,19 @@
+create database zooflasktarde;
+use zooflasktarde;
+create table animal(
+  id int primary key auto_increment,
+  nome_popular varchar(80) NOT NULL,
+  nome_cientifico varchar(60) NOT NULL,
+  habitos_noturnos LONGTEXT NOT NULL
+);
+show databases
+
+SELECT *FROM animal;
+
+create table avaliacao(
+	id int primary key auto_increment,
+    texto text not null,
+    polaridade float
+);
+
+SELECT *FROM avaliacao;
